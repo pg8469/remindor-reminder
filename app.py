26847b62639198c8534c4eea1039611e8319aee4
pg8469 <43347426+pg8469@users.noreply.github.com>
2020-06-20 14:00:25 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from myproject import app
 from background_script import scheduler
+import os
 
     
 if __name__ == "__main__":
@@ -10,4 +11,6 @@ if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port,use_reloader=False)
     
+    
+
     
